1067dad128658bda667ea25d250dc21990d59693
Skadz <46767289+skadz108@users.noreply.github.com>
2025-10-03 20:11:54 -0700

    try this lung

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
 		),
 	],
 	dependencies: [
-		.package(url: "https://github.com/krzyzanowskim/OpenSSL", from: "3.3.3001")
+		.package(url: "https://github.com/krzyzanowskim/OpenSSL", from: "1.1.2301")
 	],
 	targets: [
 		.target(
